3de16c58822d2cdbcfb22977c197d6fcb0929fb9
Vladyslav Aviedov <vladaviedov@protonmail.com>
2024-02-25 01:04:32 -0500

    settings file

--- dns/main.go
+++ dns/main.go
@@ -1,29 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/miekg/dns"
 )
 
 func main() {
-	go httpServer()
-	dnsServer()
-}
+	loadSettings()
 
-func httpServer() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", ShowDevices).Methods(http.MethodGet)
 	router.HandleFunc("/", ProcessReport).Methods(http.MethodPost)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	if Config.HTTP.Use {
+		go httpServer(router)
+	}
+	if Config.HTTPS.Use {
+		go httpsServer(router)
+	}
+
+	dnsServer()
+}
+
+func loadSettings() {
+	content, err := os.ReadFile("settings.json")
+	if err != nil {
+		log.Fatal("Failed to open 'settings.json'. Config file is required")
+	}
+
+	err = json.Unmarshal(content, &Config)
+	if err != nil {
+		log.Fatal("Failed to parse config file")
+	}
+
+	log.Println("Server config loaded")
+}
+
+func httpServer(router *mux.Router) {
+	log.Fatal(http.ListenAndServe(Config.HTTP.Port, router))
+}
+
+func httpsServer(router *mux.Router) {
+	log.Fatal(http.ListenAndServeTLS(
+		Config.HTTPS.Port,
+		Config.HTTPS.CertFile,
+		Config.HTTPS.KeyFile,
+		router,
+	))
 }
 
 func dnsServer() {
 	server := &dns.Server{
-		Addr: ":8081",
+		Addr: Config.DNS.Port,
 		Net: "udp",
 		UDPSize: 25565,
 		Handler: dns.HandlerFunc(ServeDNS),
--- dns/shared.go
+++ dns/shared.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+type Settings struct {
+	HTTP struct {
+		Use bool `json:"use"`
+		Port string `json:"port"`
+	} `json:"http"`
+	HTTPS struct {
+		Use bool `json:"use"`
+		Port string `json:"port"`
+		CertFile string `json:"certFile"`
+		KeyFile string `json:"keyFile"`
+	} `json:"https"`
+	DNS struct {
+		Port string `json:"port"`
+	} `json:"dns"`
+}
+
 type DeviceData struct {
 	Identifier string `json:"identifier"`
 	MAC string `json:"mac"`
@@ -12,5 +28,6 @@ type DeviceData struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
+var Config Settings
 var DeviceMap = make(map[string]DeviceData)
 var DeviceMutex sync.Mutex
